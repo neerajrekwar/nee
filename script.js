a30c16f8e9b160bd75683496dba3728456649eea
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-06-04 17:29:30 +0530

    gdf

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-if(today < yesterday) {
+if (today < yesterday) {
     self.improve()
 } else {
     keepGoing()
 }
 console.log()
 
-function generateOTP() { 
+function generateOTP() {
 
     // Declare a digits variable  
 
     // which stores all digits 
 
-    var digits = '012345678aszdxcfvgbhnjmk'; 
+    var digits = '012345678aszdxcfvgbhnjmk';
 
-    let OTP = ''; 
+    let OTP = '';
 
-    for (let i = 0; i < 6; i++ ) { 
+    for (let i = 0; i < 6; i++) {
 
-        OTP += digits[Math.floor(Math.random() * 20)]; 
+        OTP += digits[Math.floor(Math.random() * 20)];
 
-    } 
+    }
 
-    return OTP; 
-} 
-const date = new Date();
+    return OTP;
+}
 
-let currentDate = date.toJSON()
-console.log(currentDate)
+const date = get.elmentById("#nee"); {
+
+    let currentDate = date.toJSON()
+    console.log(currentDate)
+}
