83df81ff72bc1d790aec27e59a636ad4d14e3a48
neerajrekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-07-02 21:06:22 +0530

    3 files modified

--- tools/script.js
+++ tools/script.js
@@ -1,19 +1,23 @@
-var nee = document.getElementById("password");
-        function generateP() {
-            var pass = '';
-            var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-                    'abcdefghijklmnopqrstuvwxyz0123456789@#$';
-              
-            for (let i = 1; i <= 8; i++) {
-                var char = Math.floor(Math.random()
-                            * str.length + 1);
-                  
-                pass += str.charAt(char)
-            }
-              
-            return pass;
-        }
-          
-        function gfg_Run() {
-            el_down.innerHTML = generateP();
-        }
+//javascript
+const  passwordBox = document.getElementById("password");
+const lenght = 4;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abscefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "~!@#$%^&*()_+-=>[]|\<,.?/':;{}";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
