6580ff5d4d33303dad76e4c35ca8e1d1927c9213
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-08-16 19:48:00 +0530

    afs

--- tools/tools-script.js
+++ tools/tools-script.js
@@ -1,29 +1,2 @@
 // tools-script.js
 
-var randomPassword = function (length) {
-    chars = "!@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    var pass = "";
-    for (x = 0; x < length; x++) {
-      i = Math.floor(Math.random() * 62);
-      pass += chars.charAt(i);
-    }
-    $("#output").text(pass);
-  };
-  
-  $("button").on("click", function () {
-    var length = $("input").val();
-    if (length <= 0) {
-      $("#output").show();
-      $("#output").text("Enter a number more than 0.");
-    } else {
-      $("#output").show();
-      randomPassword(length);
-    }
-  });
-  
-  $("#info").on("click", function () {
-    $("#output").show("");
-    $("#output").text(
-      "You can use the up and down arrow keys to change the value."
-    );
-  });
