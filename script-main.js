54cc0de80061367b1d92190308960167e7b2531e
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-08-25 13:47:56 +0530

    fa

--- script-main.js
+++ script-main.js
@@ -10,31 +10,5 @@ function screen() {
 document.getElementById("screen-fit").innerHTML = 'Screen: ' + myWidth + "x" + myHeight + ' px';
 }
 
-var randomPassword = function (length) {
-  chars = "!@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  var pass = "";
-  for (x = 0; x < length; x++) {
-    i = Math.floor(Math.random() * 62);
-    pass += chars.charAt(i);
-  }
-  $("#output").text(pass);
-};
 
-$("button").on("click", function () {
-  var length = $("input").val();
-  if (length <= 0) {
-    $("#output").show();
-    $("#output").text("Enter a number more than 0.");
-  } else {
-    $("#output").show();
-    randomPassword(length);
-  }
-});
-
-$("#info").on("click", function () {
-  $("#output").show("");
-  $("#output").text(
-    "You can use the up and down arrow keys to change the value."
-  );
-});
 // const insteadIAmOk = new  Accepted("asal mein","ek tarfa",["..."])
