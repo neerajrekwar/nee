26d4ba8ae376064485c7f68a0b4610fbe3c0bcba
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-10-26 06:32:18 +0000

    dfa

--- script-main.js
+++ script-main.js
@@ -1,6 +1,6 @@
 // // Javascript
 
-window.onresize = screen;
+/* window.onresize = screen;
 window.onload = screen;
 
 function screen() {
@@ -9,6 +9,12 @@ function screen() {
 
 document.getElementById("screen-fit").innerHTML = 'Screen: ' + myWidth + "x" + myHeight + ' px';
 }
+*/
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
+}
 // const insteadIAmOk = new  Accepted("asal mein","ek tarfa",["..."])
